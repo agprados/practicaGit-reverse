26280015af0dc68871a58f35674be28681477ed6
agprados <aidagp@uma.es>
2015-03-07 14:13:15 +0100

    testing en GitAux

--- GitAux.java
+++ GitAux.java
@@ -21,4 +21,9 @@ public class GitAux {
         // Comentario incluido para pruebas
 	System.out.println("Aqui escribimos todos");
     }
+
+    public void testGitAux(){
+    	// metodo rama testing
+    	System.out.println("Metodo de testing");
+    }
 }
